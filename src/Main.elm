d8e5645cece368228a4213b22d4168ce0ca0421a
Humay <crypto347@gmail.com>
2020-05-23 12:39:32 +0400

    init commit

--- src/Main.elm
+++ src/Main.elm
@@ -1,149 +1,65 @@
 module Main exposing (..)
 
-import Browser
+import Browser exposing (Document, UrlRequest)
+import Browser.Navigation exposing (Key)
+import Html exposing (Html)
+import Url exposing (Url)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+-- MODEL
 
-import Http 
+type alias Flags = 
+    {}
+
+type alias Model = 
+    { name: String }
+
+type Msg 
+    = NoOp
+
+init : Flags -> Url -> Key -> (Model, Cmd Msg)
+init flags url key = 
+    let 
+        _=
+           Debug.log "url" url
+    in
+    ( Model "Jack", Cmd.none )
+
+-- VIEW
+
+view : Model -> Document Msg
+view model = 
+    { title = "Distinctly Average"
+    , body = 
+        [ Html.p [] [Html.text "hello world"]
+        ]
+    }
+
+-- UPDATE
+
+onUrlRequest : UrlRequest -> Msg
+onUrlRequest urlRequest = 
+    NoOp
+
+onUrlChange : Url -> Msg
+onUrlChange url = 
+    NoOp
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = 
+    case msg of 
+        NoOp -> 
+            ( model, Cmd.none )
+
 
 -- MAIN
 
-main : Program () Model Msg
 main =
-    Browser.element
+    Browser.application
         { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
-        }
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
+        }      
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( { nicknames = []
-      , errorMessage = Nothing
-      }
-    , Cmd.none
-    )        
-
--- MODEL
-    
-type alias Model =
-    { nicknames : List String
-    , errorMessage : Maybe String
-    }
-
-
--- UPDATE
-
-type Msg
-    = SendHttpRequest
-    | DataReceived (Result Http.Error String)
-
-url : String
-url =
-    "http://localhost:5016/old-school.txt"
-
-
-getNicknames : Cmd Msg
-getNicknames =
-    Http.get
-        { url = url
-        , expect = Http.expectString DataReceived
-        }
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        SendHttpRequest ->
-            ( model, getNicknames )
-            
-        DataReceived (Ok nicknamesStr) ->
-            let
-                nicknames =
-                    String.split "," nicknamesStr
-            in
-            ( { model | nicknames = nicknames }, Cmd.none )
-
-        DataReceived (Err httpError) ->
-            ( { model
-                | errorMessage = Just (buildErrorMessage httpError)
-              }
-            , Cmd.none
-            )
-
-buildErrorMessage : Http.Error -> String
-buildErrorMessage httpError =
-    case httpError of
-        Http.BadUrl message ->
-            message
-
-        Http.Timeout ->
-            "Server is taking too long to respond. Please try again later."
-
-        Http.NetworkError ->
-            "Unable to reach server."
-
-        Http.BadStatus statusCode ->
-            "Request failed with status code: " ++ String.fromInt statusCode
-
-        Http.BadBody message ->
-            message
-
--- VIEW
-
--- view : Model -> Html Msg
--- view model =
---     div []
---         [ button [ onClick SendHttpRequest ]
---             [ text "Get data from server" ]
---         , h3 [] [ text "Old School Main Characters" ]
---         , ul [] (List.map viewNickname model.nicknames)
---         ]
-
-
--- viewNickname : String -> Html Msg
--- viewNickname nickname =
---     li [] [ text nickname ]
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ button [ onClick SendHttpRequest ]
-            [ text "Get data from server" ]
-        , viewNicknamesOrError model
-        ]
-
-viewNicknamesOrError : Model -> Html Msg
-viewNicknamesOrError model =
-    case model.errorMessage of
-        Just message ->
-            viewError message
-
-        Nothing ->
-            viewNicknames model.nicknames
-
-viewError : String -> Html Msg
-viewError errorMessage =
-    let
-        errorHeading =
-            "Couldn't fetch nicknames at this time."
-    in
-        div []
-            [ h3 [] [ text errorHeading ]
-            , text ("Error: " ++ errorMessage)
-            ]
-
-viewNicknames : List String -> Html Msg
-viewNicknames nicknames =
-    div []
-        [ h3 [] [ text "Old School Main Characters" ]
-        , ul [] (List.map viewNickname nicknames)
-        ]
-
-
-viewNickname : String -> Html Msg
-viewNickname nickname =
-    li [] [ text nickname ]
